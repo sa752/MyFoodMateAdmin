feb1c1fb2acf9e5386cee27fdf399257b799fd74
Baraka Mulumia <barakamulumia@gmail.com>
2021-07-27 16:14:07 +0300

    Update LoginActivity.kt
    Added onClick Listener to the button when clicked goes to main activity

--- app/src/main/java/com/baraka/foodmate/LoginActivity.kt
+++ app/src/main/java/com/baraka/foodmate/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.baraka.foodmate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login_activty.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_activty)
+        loginUser()
+    }
+
+    private fun loginUser() {
+        login.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
