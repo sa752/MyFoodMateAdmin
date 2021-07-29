df4b3e4848a4704d632786d16ab69e51f14a7332
Baraka Mulumia <barakamulumia@gmail.com>
2021-07-29 15:36:44 +0300

    Update AddMealFragment.kt
    Implemented functionality for uploading image as well as data about a meal

--- app/src/main/java/com/baraka/foodmate/AddMealFragment.kt
+++ app/src/main/java/com/baraka/foodmate/AddMealFragment.kt
@@ -5,16 +5,22 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.text.TextUtils.isEmpty
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.fragment_add_meal.*
 import java.util.*
 
 class AddMealFragment : Fragment() {
-  private val mediaRequestCode:Int = 111
+    private val mediaRequestCode: Int = 111
     private lateinit var filePath: Uri
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,36 +31,79 @@ class AddMealFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //var uniqueID = UUID.randomUUID().toString()
-        add_meal_Button.setOnClickListener {
-            getData()
-        }
-        editMealImage.setOnClickListener{
+        editMealImage.setOnClickListener {
             choosePicture()
         }
+        uploadData()
 
     }
 
-   private  fun choosePicture(){
-        var i = Intent()
+    private fun choosePicture() {
+        val i = Intent()
         i.type = "image/*"
         i.action = Intent.ACTION_GET_CONTENT
-        startActivityForResult(Intent.createChooser(i, "Choose Picture"), mediaRequestCode )
-    }
-
-    private fun getData() {
-            val mealName = editMealTitle.text.toString().trim()
-            val mealDesc = editMealDescription.text.toString().trim()
-            val mealPrice = editPrice.text.toString().trim()
+        startActivityForResult(Intent.createChooser(i, "Choose Picture"), mediaRequestCode)
     }
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == mediaRequestCode && resultCode == Activity.RESULT_OK && data !=null){
+        if (requestCode == mediaRequestCode && resultCode == Activity.RESULT_OK && data != null) {
             filePath = data.data!!
-            var bitmap = MediaStore.Images.Media.getBitmap(activity?.contentResolver,filePath)
+            val bitmap = MediaStore.Images.Media.getBitmap(activity?.contentResolver, filePath)
             editMealImage.setImageBitmap(bitmap)
+        }
+    }
+
+    private fun uploadData() {
+        add_meal_Button.setOnClickListener {
+
+            val mealName = editMealTitle.text.toString().trim()
+            val mealDesc = editMealDescription.text.toString().trim()
+            val mealPrice = editPrice.text.toString().trim()
+
+            if (isEmpty(mealName)) {
+                editMealTitle.error = "Name for the meal is required"
+                editMealTitle.requestFocus()
+                return@setOnClickListener
+            }
+            if (isEmpty(mealPrice)) {
+                editPrice.error = "Price for the meal is required"
+                editPrice.requestFocus()
+                return@setOnClickListener
+            }
+            if (isEmpty(mealDesc)) {
+                editMealDescription.error = "A short description for the meal is required"
+                editMealDescription.requestFocus()
+                return@setOnClickListener
+            }
+            val uniqueID = UUID.randomUUID().toString()
+            if (filePath != null) {
+                uploadProgressBar.visibility = View.VISIBLE
+                val imageRef =
+                    FirebaseStorage.getInstance().reference.child("meal_images/${uniqueID}")
+                imageRef.putFile(filePath)
+                    .addOnSuccessListener {
+                        uploadProgressBar.visibility = View.INVISIBLE
+                        Toast.makeText(context, "Image uploaded", Toast.LENGTH_LONG).show()
+                    }
+                    .addOnFailureListener { exception ->
+                        uploadProgressBar.visibility = View.INVISIBLE
+                        Toast.makeText(context, exception?.message, Toast.LENGTH_LONG).show()
+                    }
+
+                val dataRef =
+                    FirebaseDatabase.getInstance().reference.child("meals_data/${uniqueID}")
+                dataRef.child("name").setValue(mealName)
+                dataRef.child("price").setValue(mealPrice)
+                dataRef.child("desc").setValue(mealDesc)
+                startActivity(Intent(context, MainActivity::class.java))
+            } else {
+                Toast.makeText(context, "Image not selected", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+
         }
     }
 
