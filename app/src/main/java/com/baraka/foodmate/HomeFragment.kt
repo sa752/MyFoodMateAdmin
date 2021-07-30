0e41356567522424585f284e2eca32dc0b48552f
Baraka Mulumia <barakamulumia@gmail.com>
2021-07-30 16:55:48 +0300

    Update HomeFragment.kt
    Added recycler view

--- app/src/main/java/com/baraka/foodmate/HomeFragment.kt
+++ app/src/main/java/com/baraka/foodmate/HomeFragment.kt
@@ -5,29 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.baraka.foodmate.adapters.MealListAdapter
+import com.baraka.foodmate.models.MealItem
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    //initialize firebase
+    private var databaseReference:DatabaseReference? = null
+    private  var databaseListener:  ValueEventListener? = null
+    private  lateinit var listOfMeals: MutableList<MealItem>
+    private lateinit var mealAdapater: MealListAdapter
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +30,45 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        listOfMeals = arrayListOf()
+        getData()
+        home_page_recycler_view.apply {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            mealAdapater = MealListAdapter(context, listOfMeals )
+            adapter = mealAdapater
+        }
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        databaseReference?.removeEventListener(databaseListener!!)
+    }
+
+ private  fun getData(){
+       databaseReference = FirebaseDatabase.getInstance().getReference("meals_data")
+       databaseReference!!.addValueEventListener(object : ValueEventListener {
+
+           override fun onDataChange(snapshot: DataSnapshot) {
+               if(snapshot.exists()){
+                   for ( mealItem in snapshot.children){
+                       val mealItemData = mealItem.getValue(MealItem::class.java)
+                       mealItemData!!.key = mealItem.key
+                       listOfMeals.add(mealItemData)
+                   }
+                   //unsure
+                   home_page_recycler_view.adapter = MealListAdapter(requireContext(),listOfMeals )
+               }
+
+           }
+
+           override fun onCancelled(error: DatabaseError) {
+               Toast.makeText(context, error.message, Toast.LENGTH_LONG).show()
+           }
+
+       })
+    }
+
 }
