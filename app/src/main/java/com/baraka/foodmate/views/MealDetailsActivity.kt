09492f8b30f0a37d51a3043db028512251bef660
Baraka Mulumia <barakamulumia@gmail.com>
2021-07-30 16:56:49 +0300

    Create MealDetailsActivity.kt

---
+++ app/src/main/java/com/baraka/foodmate/views/MealDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.baraka.foodmate.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.baraka.foodmate.R
+
+class MealDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meal_details)
+    }
+}
